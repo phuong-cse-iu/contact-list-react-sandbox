d29f388f100aef8bfcc221e9611ca58d80b6e694
phuong <trantuanphuong19941994@gmail.com>
2018-09-24 22:55:41 +0700

    First component of React

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Phuong</h4>
+        <ul>
+          <li>Email: phuong@example.com</li>
+          <li>Phone: 0908627335</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
